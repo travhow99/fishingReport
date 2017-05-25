79d1b82304f5aa06d487aa5ce0024b29054ed4b5
Travis <travis@travishowell.net>
2017-05-25 16:19:06 -0600

    Latest work

--- Hello.py
+++ Hello.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-from stpetereport import getStPete
-
 @app.route('/')
 def index():
    return render_template('index.html')
@@ -10,20 +8,177 @@ def index():
 import urllib
 from bs4 import BeautifulSoup
 
-riverList = {'Arkansas River': 'Arkansas_River', 'Big Thompson River': 'Big_Thompson_River'}#, 'Blue River': 'Blue_River', 'Boulder Creek': 'Boulder_Creek', 'Cache La Poudre River': 'Cache_La_Poudre_River', 'Clear Creek': 'Clear_Creek', 'Colorado River': 'Colorado_River', 'Fryingpan River': 'Fryingpan_River', 'Gunnison Taylor River': 'Gunnison_Taylor_River', 'North Platte River': 'North_Platte_River', 'Roaring Fork River': 'Roaring_Fork_River', 'Rocky Mountain National Park': 'Rocky_Mtn_Nat_Park', 'Saint Vrain River': 'Saint_Vrain_River', 'South Boulder Creek': 'South_Boulder_Creek', 'South Platte River': 'South_Platte_River', 'Yampa River': 'Yampa_River'}
-@app.route('/bigthompson')
-def getData():
-    for riverName, riverLink in riverList.items():
-        url = "http://www.rockymtanglers.com/" + riverLink + ".riv"
+#Today's work
+class River:
+    def __init__(self, name, peteLink, rockyLink):
+        self.name = name
+        self.peteLink = peteLink
+        self.rockyLink = rockyLink
+
+    def getStPete(self):
+            url = "https://stpetes.com/blog/river-reports/" + self.peteLink
+            html = urllib.urlopen(url).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            report = soup.select('.post-content p')
+            lines = [span.get_text() for span in report]
+            #print riverName + ": "
+            stPeteCredit = "<p class='credit'>Report courtesy of <a href='https://stpetes.com/'>St. Peter's Fly Shop</a> in Fort Collins, Colorado.</p>"
+
+            if self.name == "Big Thompson River" or self.name == "Cache La Poudre River":
+                if self.name == "Cache La Poudre River":
+                    self.stPeteReport = lines[3] + lines[4] + lines[5]
+                else:
+                    self.stPeteReport = lines[4] + lines[5] + lines[6]
+            elif self.name == "Rocky Mountain National Park" or self.name == "Encampment River" or self.name == "Laramie":
+                self.stPeteReport = lines[2]
+            elif self.name == "Upper North Platte":
+                self.stPeteReport = lines[1]
+            elif self.name == "Gray Reef River":
+                self.stPeteReport = lines[2] + lines[3] + lines[4]
+
+
+    def getRocky(self):
+        rockyCredit = "<p class='credit'>Report courtesy of <a href='http://www.rockymtanglers.com/'>Rocky Mountain Anglers</a> in Boulder, Colorado.</p>"
+
+        url = "http://www.rockymtanglers.com/" + self.rockyLink + ".riv"
         html = urllib.urlopen(url).read()
         soup = BeautifulSoup(html, 'html.parser')
         report = soup.find_all('span', {"class":"report"})
 
         lines = [span.get_text() for span in report]
-        #update = lines.split("Updated",1)[1]
+        self.rockyReport = lines[-1]
+        #return render_template('rivers.html', river = riverName, report = lines[0])
+ark = River("Arkansas River", "", "Arkansas_River")
+ark.getRocky()
+@app.route("/arkansas_river")
+def arkData():
+    return render_template('rivers.html', river = ark.name, rockyMtnReport = ark.rockyReport)
 
 
-        return render_template('rivers.html', river = riverName, report = lines[0])
+blue = River("Blue River", "", "Blue_River")
+blue.getRocky()
+@app.route("/blue_river")
+def blueData():
+    return render_template('rivers.html', river = blue.name, rockyMtnReport = blue.rockyReport)
+
+boulderCrk = River("Boulder Creek", "", "Boulder_Creek")
+boulderCrk.getRocky()
+@app.route("/boulder_creek")
+def boulderCrkData():
+    return render_template('rivers.html', river = boulderCrk.name, rockyMtnReport = boulderCrk.rockyReport)
+
+bigT = River("Big Thompson River", "Big-Thompson", "Big_Thompson_River")
+bigT.getStPete()
+bigT.getRocky()
+@app.route("/big_thompson_river")
+def bigTData():
+    return render_template('rivers.html', river = bigT.name, rockyMtnReport = bigT.rockyReport, stPeteReport = bigT.stPeteReport)
+
+clearCrk = River("Clear Creek", "", "Clear_Creek")
+clearCrk.getRocky()
+@app.route("/clear_creek")
+def clearCrkData():
+    return render_template('rivers.html', river = clearCrk.name, rockyMtnReport = clearCrk.rockyReport)
+
+colorado = River("Colorado River", "", "Colorado_River")
+colorado.getRocky()
+@app.route("/colorado_river")
+def coloradoData():
+    return render_template('rivers.html', river = colorado.name, rockyMtnReport = colorado.rockyReport)
+
+encampment = River("Encampment River", "encampment", "")
+encampment.getStPete()
+@app.route("/encampment_river")
+def encampmentData():
+    return render_template('rivers.html', river = encampment.name, stPeteReport = encampment.stPeteReport)
+
+fryingpan = River("Fryingpan River", "", "Fryingpan_River")
+fryingpan.getRocky()
+@app.route("/frying_pan_river")
+def fryingpanData():
+    return render_template('rivers.html', river = fryingpan.name, rockyMtnReport = fryingpan.rockyReport)
+
+grayReef = River("Gray Reef River", "gray-reef", "")
+grayReef.getStPete()
+@app.route("/gray_reef_river")
+def grayReefData():
+    return render_template('rivers.html', river = grayReef.name, stPeteReport = grayReef.stPeteReport)
+
+gunnison = River("Gunnison Taylor River", "", "Gunnison_Taylor_River")
+gunnison.getRocky()
+@app.route("/gunnison_taylor_river")
+def gunnisonData():
+    return render_template('rivers.html', river = gunnison.name, rockyMtnReport = gunnison.rockyReport)
+
+laramie = River("Laramie", "laramie", "")
+laramie.getStPete()
+@app.route("/laramie_river")
+def laramieData():
+    return render_template('rivers.html', river = laramie.name, stPeteReport = laramie.stPeteReport)
+
+poudre = River("Cache La Poudre River", "cache-la-poudre", "Cache_La_Poudre_River")
+poudre.getStPete()
+poudre.getRocky()
+@app.route("/poudre_river")
+def poudreData():
+    return render_template('rivers.html', river = poudre.name, rockyMtnReport = poudre.rockyReport, stPeteReport = poudre.stPeteReport)
+
+nPlatte = River("Upper North Platte", "upper-north-platte", "North_Platte_River")
+nPlatte.getStPete()
+nPlatte.getRocky()
+@app.route("/north_platte_river")
+def nPlatteData():
+    return render_template('rivers.html', river = nPlatte.name, rockyMtnReport = nPlatte.rockyReport, stPeteReport = nPlatte.stPeteReport)
+
+roaringFork = River("Roaring Fork River", "", "Roaring_Fork_River")
+roaringFork.getRocky()
+@app.route("/roaring_fork_river")
+def roaringForkData():
+    return render_template('rivers.html', river = roaringFork.name, rockyMtnReport = roaringFork.rockyReport)
+
+rocky = River("Rocky Mountain National Park", "rocky-mountain-national-park", "Rocky_Mtn_Nat_Park")
+rocky.getStPete()
+rocky.getRocky()
+@app.route("/rocky_mtn_nat_park")
+def rockyData():
+    return render_template('rivers.html', river = rocky.name, rockyMtnReport = rocky.rockyReport, stPeteReport = rocky.stPeteReport)
+
+stVrain = River("Saint Vrain River", "Saint_Vrain_River", "")
+stVrain.getStPete()
+@app.route("/saint_vrain_river")
+def stVrainData():
+    return render_template('rivers.html', river = stVrain.name, stPeteReport = stVrain.stPeteReport)
+
+sBoulderCrk = River("South Boulder Creek", "", "South_Boulder_Creek")
+sBoulderCrk.getRocky()
+@app.route("/south_boulder_creek")
+def sBoulderCrkData():
+    return render_template('rivers.html', river = sBoulderCrk.name, rockyMtnReport = sBoulderCrk.rockyReport)
+
+sPlatte = River("South Platte River", "", "South_Platte_River")
+sPlatte.getRocky()
+@app.route("/south_platte_river")
+def sPlatteData():
+    return render_template('rivers.html', river = sPlatte.name, rockyMtnReport = sPlatte.rockyReport)
+
+stillwater = River("Stillwater", "stillwater", "")
+stillwater.getStPete()
+@app.route("/stillwater")
+def stillwaterData():
+    return render_template('rivers.html', river = stillwater.name, stPeteReport = stillwater.stPeteReport)
+
+yampa = River("Yampa River", "", "Yampa_River")
+yampa.getRocky()
+@app.route("/yampa_river")
+def yampaData():
+    return render_template('rivers.html', river = yampa.name, rockyMtnReport = yampa.rockyReport)
 
 if __name__ == '__main__':
     app.run(debug = True)
+#big t: 4 5 6
+#poudre: 3 4 5 6
+#upper north platte: 1
+# rocky : 2
+#Gray reef:2 3 4
+#encampment: 2
+#Laramie: 2
--- stpetereport.py
+++ stpetereport.py
@@ -1,35 +1,119 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
 import urllib
 from bs4 import BeautifulSoup
 
-riverList = {'Big Thompson River': 'Big-Thompson','Cache La Poudre River': 'cache-la-poudre', 'Upper North Platte': 'upper-north-platte', 'Rocky Mountain National Park': 'rocky-mountain-national-park', 'Gray Reef River': 'gray-reef', 'Encampment River': 'encampment', 'Laramie': 'laramie'}#, 'Stillwaters River': 'stillwaters'}
+#Today's work
+class River:
+    def __init__(self, name, peteLink, rockyLink):
+        self.name = name
+        self.peteLink = peteLink
+        self.rockyLink = rockyLink
 
-for riverName, riverLink in riverList.items():
-    url = "https://stpetes.com/blog/river-reports/" + riverLink
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    report = soup.select('.post-content p')
+    def getStPete(self):
+            url = "https://stpetes.com/blog/river-reports/" + self.peteLink
+            html = urllib.urlopen(url).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            report = soup.select('.post-content p')
+            lines = [span.get_text() for span in report]
+            #print riverName + ": "
+            stPeteCredit = "<p class='credit'>Report courtesy of <a href='https://stpetes.com/'>St. Peter's Fly Shop</a> in Fort Collins, Colorado.</p>"
 
-    lines = [span.get_text() for span in report]
-    print riverName + ": "
-
-    if riverName == "Big Thompson River" or riverName == "Cache La Poudre River":
-        if riverName == "Cache La Poudre River":
-            print lines[3]
-        print lines[4]
-        print lines[5]
-        if riverName == "Big Thompson River":
-            print lines[6]
-    elif riverName == "Rocky Mountain National Park" or riverName == "Encampment River" or riverName == "Laramie":
-        print lines[2]
-    elif riverName == "Upper North Platte":
-        print lines[1]
-    elif riverName == "Gray Reef River":
-        print lines[2]
-        print lines[3]
-        print lines[4]
+            if self.name == "Big Thompson River" or self.name == "Cache La Poudre River":
+                if self.name == "Cache La Poudre River":
+                    self.stPeteReport = self.name + ": " + lines[3] + lines[4] + lines[5] + stPeteCredit
+                else:
+                    self.stPeteReport = self.name + ": " + lines[4] + lines[5] + lines[6] + stPeteCredit
+            elif self.name == "Rocky Mountain National Park" or self.name == "Encampment River" or self.name == "Laramie":
+                self.stPeteReport = self.name + ": " + lines[2] + stPeteCredit
+            elif self.name == "Upper North Platte":
+                self.stPeteReport = self.name + ": " + lines[1] + stPeteCredit
+            elif self.name == "Gray Reef River":
+                self.stPeteReport = self.name + ": " + lines[2] + lines[3] + lines[4] + stPeteCredit
 
 
+    def getRocky(self):
+        rockyCredit = "<p class='credit'>Report courtesy of <a href='http://www.rockymtanglers.com/'>Rocky Mountain Anglers</a> in Boulder, Colorado.</p>"
 
+        url = "http://www.rockymtanglers.com/" + self.rockyLink + ".riv"
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        report = soup.find_all('span', {"class":"report"})
+
+        lines = [span.get_text() for span in report]
+        self.rockyReport = lines[0]
+        #return render_template('rivers.html', river = riverName, report = lines[0])
+ark = River("Arkansas River", "", "Arkansas_River")
+ark.getRocky()
+@app.route("/arkansas_river")
+def arkData():
+    return render_template('rivers.html', river = ark.name, report = ark.rockyReport)
+
+
+blue = River("Blue River", "", "Blue_River")
+blue.getRocky()
+
+boulderCrk = River("Boulder Creek", "", "Boulder_Creek")
+boulderCrk.getRocky()
+
+bigT = River("Big Thompson River", "Big-Thompson", "Big_Thompson_River")
+bigT.getStPete()
+bigT.getRocky()
+
+clearCrk = River("Clear Creek", "", "Clear_Creek")
+clearCrk.getRocky()
+
+colorado = River("Colorado River", "", "Colorado_River")
+colorado.getRocky()
+
+encampment = River("Encampment River", "encampment", "")
+encampment.getStPete()
+
+fryingpan = River("Fryingpan River", "", "Fryingpan_River")
+fryingpan.getRocky()
+
+grayReef = River("Gray Reef River", "gray-reef", "")
+grayReef.getStPete()
+
+gunnison = River("Gunnison Taylor River", "", "Gunnison_Taylor_River")
+gunnison.getRocky()
+
+laramie = River("Laramie", "laramie", "")
+laramie.getStPete()
+
+poudre = River("Cache La Poudre River", "cache-la-poudre", "Cache_La_Poudre_River")
+poudre.getStPete()
+poudre.getRocky()
+
+nPlatte = River("Upper North Platte", "upper-north-platte", "North_Platte_River")
+nPlatte.getStPete()
+nPlatte.getRocky()
+
+roaringFork = River("Roaring Fork River", "", "Roaring_Fork_River")
+roaringFork.getRocky()
+
+rocky = River("Rocky Mountain National Park", "rocky-mountain-national-park", "Rocky_Mtn_Nat_Park")
+rocky.getStPete()
+rocky.getRocky()
+
+sBoulderCrk = River("South Boulder Creek", "", "South_Boulder_Creek")
+sBoulderCrk.getRocky()
+
+sPlatte = River("South Platte River", "", "South_Platte_River")
+sPlatte.getRocky()
+
+stillwaters = River("Stillwaters", "stillwaters", "")
+stillwaters.getStPete()
+
+yampa = River("Yampa River", "", "Yampa_River")
+
+if __name__ == '__main__':
+    app.run(debug = True)
 #big t: 4 5 6
 #poudre: 3 4 5 6
 #upper north platte: 1
