bd414a85d610f5fbfb165ce9c17e0e5abb6ac5ff
Travis <travis@travishowell.net>
2017-07-16 15:28:19 -0600

    app update

--- Hello.py
+++ Hello.py
@@ -9,7 +9,7 @@ firebase = \
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   return render_template('rivers.html')
 
 import urllib
 from bs4 import BeautifulSoup
@@ -458,8 +458,7 @@ def yampaData():
 
 if __name__ == '__main__':
     app.run(debug = True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+
 
 #big t: 4 5 6
 #poudre: 3 4 5 6
